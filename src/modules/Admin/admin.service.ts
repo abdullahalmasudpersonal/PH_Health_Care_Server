cfdc7c41d5d4e967380c7550b9fa832d6d3318e9
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-03-17 00:16:02 +0600

    compleate module 26.6

--- src/modules/Admin/admin.service.ts
+++ src/modules/Admin/admin.service.ts
@@ -3,24 +3,30 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllAdminFromDB = async (parms: any) => {
+  const { searchTerm, ...filterData } = parms;
   const andCondition: Prisma.AdminWhereInput[] = [];
+  const adminSearchAbleFields = ["name", "email"];
+
+  console.log(filterData);
 
   if (parms.searchTerm) {
     andCondition.push({
-      OR: [
-        {
-          name: {
-            contains: parms.searchTerm,
-            mode: "insensitive",
-          },
+      OR: adminSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: parms.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: parms.searchTerm,
-            mode: "insensitive",
-          },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andCondition.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
         },
-      ],
+      })),
     });
   }
 
