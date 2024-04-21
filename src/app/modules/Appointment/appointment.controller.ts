e2169aec110e5a385b24e8df56e2407638756afd
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-04-22 00:04:51 +0600

    compleate module 36.1

--- src/app/modules/Appointment/appointment.controller.ts
+++ src/app/modules/Appointment/appointment.controller.ts
@@ -4,6 +4,48 @@ import httpStatus from "http-status";
 import sendResponse from "../../../shared/sendResponse";
 import { AppointmentServices } from "./appointment.services";
 import { IAuthUser } from "../../interfaces/common";
+import pick from "../../../shared/pick";
+import { appointmentFilterableFields } from "./appointment.constants";
+
+const getAllAppointment = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const filters = pick(req.query, appointmentFilterableFields);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+    const result = await AppointmentServices.getAllAppointmentFromDB(
+      filters,
+      options
+    );
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Appointment retrieval successfully",
+      meta: result.meta,
+      data: result.data,
+    });
+  }
+);
+
+const getMyAppointment = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const filters = pick(req.query, ["status", "paymentStatus"]);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+    const result = await AppointmentServices.getMyAppointmentIntoDB(
+      user as IAuthUser,
+      filters,
+      options
+    );
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Get my appointment successfully",
+      data: result,
+    });
+  }
+);
 
 const creaeAppointment = catchAsync(
   async (req: Request & { user?: IAuthUser }, res: Response) => {
@@ -23,5 +65,7 @@ const creaeAppointment = catchAsync(
 );
 
 export const AppointmentController = {
+  getAllAppointment,
   creaeAppointment,
+  getMyAppointment,
 };
--- src/app/modules/Appointment/appointment.services.ts
+++ src/app/modules/Appointment/appointment.services.ts
@@ -1,5 +1,154 @@
 import prisma from "../../../shared/prisma";
 import { v4 as uuidv4 } from "uuid";
+import { IAuthUser, IGenericResponse } from "../../interfaces/common";
+import { IPaginationOptions } from "../../interfaces/pagination";
+import { paginationHelper } from "../../../helpers/paginationHelper";
+import { Appointment, Prisma, UserRole } from "@prisma/client";
+import {
+  appointmentRelationalFields,
+  appointmentRelationalFieldsMapper,
+} from "./appointment.constants";
+
+const getMyAppointmentIntoDB = async (
+  user: IAuthUser,
+  filters: any,
+  options: IPaginationOptions
+) => {
+  const { limit, page, skip } = paginationHelper.calculatePagination(options);
+  const { ...filterData } = filters;
+
+  const andConditions: Prisma.AppointmentWhereInput[] = [];
+
+  if (user?.role === UserRole.PATIENT) {
+    andConditions.push({
+      patient: {
+        email: user?.email,
+      },
+    });
+  } else if (user?.role === UserRole.DOCTOR) {
+    andConditions.push({
+      doctor: {
+        email: user?.email,
+      },
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    const filterConditions = Object.keys(filterData).map((key) => ({
+      [key]: {
+        equals: (filterData as any)[key],
+      },
+    }));
+    andConditions.push(...filterConditions);
+  }
+
+  const whereConditions: Prisma.AppointmentWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.appointment.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : { createdAt: "desc" },
+    include:
+      user?.role === UserRole.PATIENT
+        ? { doctor: true, schedule: true }
+        : {
+            patient: {
+              include: { medicalReport: true, patientHealthData: true },
+            },
+            schedule: true,
+          },
+  });
+
+  const total = await prisma.appointment.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
+const getAllAppointmentFromDB = async (
+  filters: any,
+  options: IPaginationOptions
+): Promise<IGenericResponse<Appointment[]>> => {
+  const { limit, page, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+  const andConditions = [];
+
+  // if (searchTerm) {
+  //     andConditions.push({
+  //         OR: appointmentSearchableFields.map(field => ({
+  //             [field]: {
+  //                 contains: searchTerm,
+  //                 mode: 'insensitive',
+  //             },
+  //         })),
+  //     });
+  // }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => {
+        if (appointmentRelationalFields.includes(key)) {
+          return {
+            [appointmentRelationalFieldsMapper[key]]: {
+              email: (filterData as any)[key],
+            },
+          };
+        } else {
+          return {
+            [key]: {
+              equals: (filterData as any)[key],
+            },
+          };
+        }
+      }),
+    });
+  }
+
+  // console.dir(andConditions, { depth: Infinity })
+  const whereConditions: Prisma.AppointmentWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.appointment.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: "desc",
+          },
+    include: {
+      doctor: true,
+      patient: true,
+    },
+  });
+  const total = await prisma.appointment.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
 
 const creaeAppointmentIntoDB = async (payload: any, user: any) => {
   const today = new Date();
@@ -97,4 +246,6 @@ const creaeAppointmentIntoDB = async (payload: any, user: any) => {
 
 export const AppointmentServices = {
   creaeAppointmentIntoDB,
+  getMyAppointmentIntoDB,
+  getAllAppointmentFromDB,
 };
